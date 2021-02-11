7d3742aa491c3285a19bd4aaaaaa90a449272379
Nick Abbasi <nabbasi@merrittproperties.com>
2021-02-11 13:01:41 -0500

    calculator is working. want to add in ways to add in addtional payments

--- mort_calc.py
+++ mort_calc.py
@@ -1,5 +1,7 @@
+import pandas as pd
+
 def calc_reg_payment(rate, amount, years):
-    import pandas as pd
+
 
     n = years * 12
     r = rate/12
@@ -10,18 +12,37 @@ def calc_reg_payment(rate, amount, years):
     return(p)
 
 
+def calcing_payment_schedule(rate, total,years):
 
-rate = .03
-total = 10000
-years = 7
-months = years * 12
+    dict = {'month':[],'payment':[],'principal':[],'interest':[], 'remaining':[]}
+    months = years * 12
 
-p = calc_reg_payment(rate, total   , years)
-#print(p, p*84)
-remaining = total
-monthly_rate = rate/12
-x=1
-while x <= months:
-    remaining = remaining*(1 + monthly_rate) - p
-    print(x,remaining)
-    x+=1
+    p = calc_reg_payment(rate, total   , years)
+    #print(p, p*84)
+    remaining = total
+    monthly_rate = rate/12
+    x=1
+    while x <= months:
+
+        dict['month'].append(x)
+        dict['payment'].append(p)
+        dict['principal'].append(p - (remaining*(monthly_rate)))
+        dict['interest'].append((remaining*(monthly_rate)))
+        remaining = remaining*(1 + monthly_rate) - p
+        dict['remaining'].append(remaining)
+        #print(x,remaining)
+        x+=1
+    return(dict)
+
+
+rate = .02875
+years = 30
+total = 315400
+
+dict = calcing_payment_schedule(rate, total, years)
+
+df = pd.DataFrame(dict)
+
+df.to_excel('..\mort.xlsx', 'data')
+
+print(df)
